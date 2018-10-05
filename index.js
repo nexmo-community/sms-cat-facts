eb89fc5d60cae48376eaedd89ce374df001459d4
Steve Crow <steve.crow@vonage.com>
2018-10-04 20:51:43 -0400

    Add cancel message function.

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ require('dotenv').load();
 const API_KEY = process.env.NEXMO_API_KEY;
 const API_SECRET = process.env.NEXMO_API_SECRET;
 const NEXMO_NUMBER = process.env.NEXMO_NUMBER;
+
+const CANCEL_KEYWORDS = ["CANCEL", "STOP", "NO MORE", "HALT", "#(@$*)(@", "WHY ME", "CATS R COOL"];
+
+function generateCancelMessage() {
+    const keyword = CANCEL_KEYWORDS[Math.floor(Math.random() * CANCEL_KEYWORDS.length)];
+    return `\nReply ${keyword} to stop.`;
+}
+
+console.log(generateCancelMessage());
