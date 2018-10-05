e33ab3948a2198aebeb0ef16be3c5ed8b1661864
Steve Crow <steve.crow@vonage.com>
2018-10-04 20:46:39 -0400

    Add example.env and basic index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').load();
+
+// Constants
+const API_KEY = process.env.NEXMO_API_KEY;
+const API_SECRET = process.env.NEXMO_API_SECRET;
+const NEXMO_NUMBER = process.env.NEXMO_NUMBER;
